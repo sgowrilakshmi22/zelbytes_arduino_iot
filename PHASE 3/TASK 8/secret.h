ce07b21b599445373e0ef5369ec34494d8d26634
sgowrilakshmi22 <sgowrilakshmi22@gmail.com>
2026-06-22 11:34:13 +0530

    Update secret.h

--- PHASE 3/TASK 8/secret.h
+++ PHASE 3/TASK 8/secret.h
@@ -1,1 +1,7 @@
-zil_otalwlfxlb4ez26dhjoyfyk7xzbwuf3c
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define API_KEY "zil_otalwlfxlb4ez26dhjoyfyk7xzbwuf3c"
+#define DEVICE_ID "gowrilaksjmi_bench01"
+
+#endif
